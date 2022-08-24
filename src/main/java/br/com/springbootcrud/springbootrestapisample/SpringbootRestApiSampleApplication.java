3d4c36aebce49ca1abfa2aa9a8ee863f2caffca9
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-08-24 16:11:39 -0300

    Initial commit

---
+++ src/main/java/br/com/springbootcrud/springbootrestapisample/SpringbootRestApiSampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.springbootcrud.springbootrestapisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApiSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestApiSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springbootcrud/springbootrestapisample/SpringbootRestApiSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springbootcrud.springbootrestapisample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

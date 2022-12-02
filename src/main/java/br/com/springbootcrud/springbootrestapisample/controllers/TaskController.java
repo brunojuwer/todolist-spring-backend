52db7404129233eb239824c4f7e9af2b1bb73d80
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-12-02 17:19:08 -0300

    Ajustes em métodos do controller das Tasks

--- src/main/java/br/com/springbootcrud/springbootrestapisample/controllers/TaskController.java
+++ src/main/java/br/com/springbootcrud/springbootrestapisample/controllers/TaskController.java
@@ -8,14 +8,15 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// @CrossOrigin(origins = "http://127.0.0.1:5173")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/tasks")
+@RequestMapping("/tasks") // localhost:8800
 public class TaskController {
 
     @Autowired
     private TaskRepository taskRepository;
 
-    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping
     @ResponseBody
     public ResponseEntity<Task> save(@RequestBody Task task) {
@@ -24,7 +25,6 @@ public class TaskController {
         return new ResponseEntity<Task>(newTask, HttpStatus.CREATED);
     }
 
-    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping
     @ResponseBody
     public ResponseEntity<List<Task>> listTask() {
@@ -33,24 +33,20 @@ public class TaskController {
         return new ResponseEntity<List<Task>>(tasks, HttpStatus.OK);
     }
 
-    @CrossOrigin(origins = "http://localhost:5173")
-    @DeleteMapping
+    @DeleteMapping("/{idTask}")
     @ResponseBody
-    public ResponseEntity<String> delete(@RequestParam Long idTask) {
+    public ResponseEntity<String> delete(@PathVariable Long idTask) {
 
         taskRepository.deleteById(idTask);
 
         return new ResponseEntity<String>("Task successfully deleted", HttpStatus.OK);
     }
 
-
-    @CrossOrigin(origins = "http://localhost:5173")
-    @PutMapping
+    @PutMapping("/{idTask}")
     @ResponseBody
-    public ResponseEntity<String> update(@RequestParam Long idTask) {
+    public ResponseEntity<String> update(@PathVariable Long idTask) {
         Task getTask = taskRepository.getReferenceById(idTask);
         taskRepository.updateIsComplete(idTask, !getTask.isTaskComplete());
-
 
         return new ResponseEntity<String>("Task successfully updated", HttpStatus.OK);
     }
